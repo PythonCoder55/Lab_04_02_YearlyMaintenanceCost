8d457b54c2e2007f5a5bfcb9933bccc7a4014c2c
PythonCoder55 <edwardbarchv@gmail.com>
2023-06-01 12:45:14 -0400

    end of task 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int fallCost = 13;
+        int winterCost = 17;
+        int springCost = 12;
+        int summerCost = 9;
+        int totalCost;
+
+        totalCost = fallCost + winterCost + springCost + summerCost;
+
+        System.out.println("The total yearly maintenance costs based on the fall cost of $" + fallCost + ", the winter cost of $" +
+                winterCost + ", the spring cost of $" + springCost + " and the summer cost of $" + summerCost + " is $" + totalCost);
     }
 }
